5d264413958b91cfb0aece76fca475000cd74c02
smokehawk <steelldub@gmail.com>
2018-11-01 19:57:46 +0500

    task 2-2 cost

--- index.php
+++ index.php
@@ -9,40 +9,46 @@ $lots=[
     [
         'title'=>'2014 Rossignol District Snowboard',
         'category'=>'Доски и лыжи',
-        'cost'=>'10999',
+        'cost'=>10999,
         'imgurl'=>'img/lot-1.jpg',
     ],
     [
         'title'=>'DC Ply Mens 2016/2017 Snowboard',
         'category'=>'Доски и лыжи',
-        'cost'=>'15999',
+        'cost'=>15999,
         'imgurl'=>'img/lot-2.jpg',
     ],
     [
         'title'=>'Крепления Union Contact Pro 2015 года размер L/XL',
         'category'=>'Крепления',
-        'cost'=>'8000',
+        'cost'=>8000,
         'imgurl'=>'img/lot-3.jpg',
     ],
     [
         'title'=>'Ботинки для сноуборда DC Mutiny Charcoal',
         'category'=>'Ботинки',
-        'cost'=>'10999',
+        'cost'=>10999,
         'imgurl'=>'img/lot-4.jpg',
     ],
     [
         'title'=>'Куртка для сноуборда DC Mutiny Charcoal',
         'category'=>'Одежда',
-        'cost'=>'7500',
+        'cost'=>7500,
         'imgurl'=>'img/lot-5.jpg',
     ],
     [
         'title'=>'Маска Oakley Canopy',
         'category'=>'Разное',
-        'cost'=>'5400',
+        'cost'=>5400,
         'imgurl'=>'img/lot-6.jpg',
     ],
-]
+];
+
+function cost_format($cost) {
+    if($cost>=1000)$cost=ceil($cost);
+    return number_format($cost,0,'',' ');
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -131,7 +137,7 @@ $lots=[
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$value['cost']?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=cost_format($value['cost']);?> ₽</span>
                         </div>
                         <div class="lot__timer timer">
 
